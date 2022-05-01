99d57b3dcbb9932a9964e4a4730823557b8cd49f
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2022-05-01 10:49:15 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,112 @@
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE)
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
+radio.set_group(69)
+radio.set_transmit_serial_number(True)
+radio.set_transmit_power(7)
+enabled = 1
+hlasy = ["None"]
+x = 0
+posilani = 0 
+def votes(): 
+    global posilani, hlasy, x, enabled   
+    while posilani == 1:
+        
+        for i in range(1):
+            basic.pause(5000)
+            if hlasy[x] == "A":
+                radio.send_value("vote", 1)
+                print("A")
+            elif hlasy[x] == "B":
+                radio.send_value("vote", 2)
+                print("B")
+            elif hlasy[x] == "C":
+                radio.send_value("vote", 3)
+                print("C")
+            elif hlasy[x] == "D":
+                radio.send_value("vote", 4)
+                print("D")
+            print(hlasy[x]) 
+            posilani = 0
+            enabled = 0 
+            x = 0 
+            print(hlasy[x])         
+forever(votes)
+
+def on_button_pressed_a():
+    global enabled, hlasy, x, posilani
+    if enabled == 1:
+        if posilani == 1:
+            pass
+        else:
+            posilani = 1  
+        hlasy.append("A")
+        x += 1
+        print(x)
+    else:
+        pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    global enabled, hlasy, x, posilani
+    if enabled == 1:
+        if posilani == 1:
+            pass
+        else:
+            posilani = 1    
+        hlasy.push("B")
+        x += 1
+        print(x)
+
+    else:
+        pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+def on_pin_pressed_p1():
+    global enabled, x, hlasy, posilani
+    if enabled == 1:
+        if posilani == 1:
+            pass
+        else:
+            posilani = 1    
+        hlasy.push("C")
+        x += 1
+        print(x)
+    else:
+        pass
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+def on_pin_pressed_p2():
+    global enabled, hlasy, x, posilani
+    if enabled == 1:
+        if posilani == 1:
+            pass
+        else:
+            posilani = 1    
+        hlasy.push("D")
+        x += 1
+        print(x)
+    else:
+        pass
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def on_received_value(name, value):
+    global enabled
+    if name == "vote" and value == 1:
+
+        basic.show_icon(IconNames.HEART)
+    if name == "enabled" and value == 1:
+        enabled = 1
+        
+    if name == "enabled" and value == 0:
+        enabled = 0
+        
+    if name == "ack" and value == control.device_serial_number():
+        print(value)
+        basic.show_icon(IconNames.HEART)
+        basic.pause(300)
+        basic.clear_screen()
+radio.on_received_value(on_received_value)
+
+def on_received_number(receivedNumber):
+    basic.show_icon(IconNames.HEART)
+    basic.pause(3000)
+    basic.clear_screen()
+radio.on_received_number(on_received_number)
+print(control.device_serial_number())
---
+++ main.ts
@@ -0,0 +1,132 @@
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+radio.setGroup(69)
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(7)
+let enabled = 1
+let hlasy = ["None"]
+let x = 0
+let posilani = 0
+forever(function votes() {
+    
+    while (posilani == 1) {
+        for (let i = 0; i < 1; i++) {
+            basic.pause(5000)
+            if (hlasy[x] == "A") {
+                radio.sendValue("vote", 1)
+                console.log("A")
+            } else if (hlasy[x] == "B") {
+                radio.sendValue("vote", 2)
+                console.log("B")
+            } else if (hlasy[x] == "C") {
+                radio.sendValue("vote", 3)
+                console.log("C")
+            } else if (hlasy[x] == "D") {
+                radio.sendValue("vote", 4)
+                console.log("D")
+            }
+            
+            console.log(hlasy[x])
+            posilani = 0
+            enabled = 0
+            x = 0
+            console.log(hlasy[x])
+        }
+    }
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (enabled == 1) {
+        if (posilani == 1) {
+            
+        } else {
+            posilani = 1
+        }
+        
+        hlasy.push("A")
+        x += 1
+        console.log(x)
+    } else {
+        
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (enabled == 1) {
+        if (posilani == 1) {
+            
+        } else {
+            posilani = 1
+        }
+        
+        hlasy.push("B")
+        x += 1
+        console.log(x)
+    } else {
+        
+    }
+    
+})
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
+    if (enabled == 1) {
+        if (posilani == 1) {
+            
+        } else {
+            posilani = 1
+        }
+        
+        hlasy.push("C")
+        x += 1
+        console.log(x)
+    } else {
+        
+    }
+    
+})
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    
+    if (enabled == 1) {
+        if (posilani == 1) {
+            
+        } else {
+            posilani = 1
+        }
+        
+        hlasy.push("D")
+        x += 1
+        console.log(x)
+    } else {
+        
+    }
+    
+})
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+    if (name == "vote" && value == 1) {
+        basic.showIcon(IconNames.Heart)
+    }
+    
+    if (name == "enabled" && value == 1) {
+        enabled = 1
+    }
+    
+    if (name == "enabled" && value == 0) {
+        enabled = 0
+    }
+    
+    if (name == "ack" && value == control.deviceSerialNumber()) {
+        console.log(value)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(300)
+        basic.clearScreen()
+    }
+    
+})
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(3000)
+    basic.clearScreen()
+})
+console.log(control.deviceSerialNumber())
